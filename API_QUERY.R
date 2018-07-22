3030118015455910d741505456b21146f99943ce
Mattias99 <tillmattias@gmail.com>
2018-07-22 23:00:49 +0200

    Follows lintr syntax

--- API_QUERY.R
+++ API_QUERY.R
@@ -11,16 +11,11 @@
 # kommunid 0180 : Stockholm 
 #======================================
 
-# /platsannonser/matchning?lanid={M}&kommunid={M}&yrkesid={M}&
-# nyckelord ={M}&sida={V}&antalrader={V}
 
 AMS_QUERY <- function(lan, kommun, text){
-  response <- AMS_API(path = 'af/v0/platsannonser/matchning',
-                      q = list(lanid=lan,
+  response <- AMS_API(path = "af/v0/platsannonser/matchning",
+                      q = list(lanid = lan,
                                kommunid = kommun,
                                nyckelord = text))
-  
-  #response <- do.call(rbind.data.frame, response$matchningslista$matchningdata)
-  
   return(response)
 }
