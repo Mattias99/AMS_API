f80d171ab069a27ff89490300f1b59826e09e98b
Mattias99 <tillmattias@gmail.com>
2018-07-22 22:51:32 +0200

    Follows lintr syntax

--- API_GET.R
+++ API_GET.R
@@ -1,25 +1,24 @@
 # Connection to AMS API
 
 #### PACKAGES ####
+
+
 library(httr)
 library(jsonlite)
 
+
 #### API function ####
 
+
 AMS_API <- function(path, q) {
-  link <- modify_url(url = "http://api.arbetsformedlingen.se/", query = q, path = path)
+  url <- "http://api.arbetsformedlingen.se/"
+  link <- httr::modify_url(url = url, query = q, path = path)
   print(link)
-  
-  response <- GET(url = link,
-      add_headers('Accept-Language' = 'sv'))
-  
+  response <- GET(url = link, add_headers("Accept-Language" = "sv"))
   print(response$headers[9])
-
-  # if (response$headers[9] != "application/json;charset=utf-8") {
-  #   stop("API did not return json", call. = FALSE)
-  # }
-  
-  fromJSON(content(response, 'text'), simplifyVector = FALSE)
-  
+   if (response$headers[9] != "application/json;charset=utf-8") {
+    stop("API did not return json", call. = FALSE)
+  }
+  jsonlite::fromJSON(jsonlite::content(response, "text"),
+                     simplifyVector = FALSE)
 }
-
