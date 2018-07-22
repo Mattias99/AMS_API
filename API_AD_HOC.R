6dd970baaaa0562caf9fe3fd8a498463a5e3f61d
Mattias99 <tillmattias@gmail.com>
2018-07-22 23:06:10 +0200

    Follows lintr syntax

--- API_AD_HOC.R
+++ API_AD_HOC.R
@@ -1,4 +1,6 @@
-# PACKAGE
+#### PACKAGE ####
+
+
 library(jsonlite)
 
 
@@ -15,18 +17,20 @@ library(jsonlite)
 # kommunid 0180 : Stockholm 
 #======================================
 
+
 #======================================
 # AD-HOC RESPONSE
 # AMS_API
 #======================================
 
-respSok <- AMS_API(path = 'af/v0/platsannonser/soklista/kommuner',
-                   q = list(lanid=10))
 
-fromJSON(content(respSok, 'text'), simplifyVector = TRUE)
-http_type(respSok)
+respsok <- AMS_API(path = "af/v0/platsannonser/soklista/kommuner",
+                   q = list(lanid = 10))
 
-respMatch <- AMS_API(path = 'af/v0/platsannonser/22503390', q = NULL)
+jsonlite::fromJSON(jsonlite::content(respsok, "text"), simplifyVector = TRUE)
+http_type(respsok)
+
+respmatch <- AMS_API(path = "af/v0/platsannonser/22503390", q = NULL)
 
 # This works
 # Previous Error: Bad header parameter, add language to 'sv'
@@ -34,8 +38,8 @@ respMatch <- AMS_API(path = 'af/v0/platsannonser/22503390', q = NULL)
 
 response <-
   GET(
-    'http://api.arbetsformedlingen.se/af/v0/platsannonser/22503390',
-    add_headers('Accept-Language' = 'sv')
+    "http://api.arbetsformedlingen.se/af/v0/platsannonser/22503390",
+    add_headers("Accept-Language" = "sv")
   )
 
 status_code(response)
@@ -48,9 +52,8 @@ parsed <- jsonlite::fromJSON(content(response, "text"), simplifyVector = FALSE)
 # AMS_QUERY
 #======================================
 
-norrkoping <- AMS_QUERY(lan = 05,kommun = 0581, text = 'statistik')
+norrkoping <- AMS_QUERY(lan = 05, kommun = 0581, text = "statistik")
 
 test <- do.call(rbind.data.frame, norrkoping$matchningslista$matchningdata)
 
 norrkoping
-
