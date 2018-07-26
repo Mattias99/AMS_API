50bdf6feb880ef8586b0c88319015375e3cadf47
Mattias99 <tillmattias@gmail.com>
2018-07-26 12:13:18 +0200

    Combine subsets of data

--- API_TRANSFORM.R
+++ API_TRANSFORM.R
@@ -16,18 +16,19 @@ sthlmdata <- do.call(rbind.data.frame, stockholm$matchningslista$matchningdata) 
   as.tibble()
 
 
-api_df <- select(norrdata,
-                 annonsid,
-                 annonsrubrik,
-                 yrkesbenamning,
-                 yrkesbenamningId, # Uppercase in recived data
-                 arbetsplatsnamn,
-                 kommunnamn,
-                 lan,
-                 publiceraddatum,
-                 sista_ansokningsdag) %>%
+api_df <- bind_rows(norrdata, sthlmdata) %>% 
+  select(
+         annonsid,
+         annonsrubrik,
+         yrkesbenamning,
+         yrkesbenamningId, # Uppercase in recived data
+         arbetsplatsnamn,
+         kommunnamn,
+         lan,
+         publiceraddatum,
+         sista_ansokningsdag) %>%
   mutate(publiceraddatum = ymd(str_sub(publiceraddatum,
-                                       start = 1L, end = 10L)),
+                                           start = 1L, end = 10L)),
          sista_ansokningsdag = ymd(str_sub(sista_ansokningsdag,
                                            start = 1L, end = 10L))) %>%
   filter(yrkesbenamning == "Statistiker")
